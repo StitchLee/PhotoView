1d61fe40f5ab766cba6c9b1fd1a661d6fe20872b
StitchLee <1763724062@qq.com>
2019-02-19 10:40:50 +0800

    first Commit.

---
+++ app/src/main/java/com/stitch/photoviewdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.stitch.photoviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+ * PhotoView+Glide+Dialog+ViewPager:打造轻量级的图片浏览方案
+ * @date 20190219
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/stitch/photoviewdemo/base/Config.java
@@ -0,0 +1,10 @@
+package com.stitch.photoviewdemo.base;
+
+/**
+ * Created by StitcheLee on 2019/2/19.
+ */
+
+public class Config {
+    public static int EXACT_SCREEN_HEIGHT;
+    public static int EXACT_SCREEN_WIDTH;
+}
